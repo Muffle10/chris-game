badf436a6f4c15a5f829d9d9558c79459a84385c
Family <Family@Batapreneurs-iMac.local>
2022-10-01 20:42:49 -0700

    Added dev files and began writing game

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
 #include <raylib.h>
+int main(){
+	const int screenWidth = 800;
+   	const int screenHeight = 450;
+	const char* title = "chris game";
+   	InitWindow(screenWidth, screenHeight, title);
+   	SetTargetFPS(60);
+	while (!WindowShouldClose()){
+		BeginDrawing();
+          	ClearBackground(RAYWHITE);
+		EndDrawing();
+   
+	}
+	CloseWindow(); 
+	return 0;
+}
